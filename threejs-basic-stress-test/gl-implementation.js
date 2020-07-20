98f073969efa9f2495819cc95624021467c43666
James Hobin <jhobin@ptc.com>
2020-07-20 14:57:56 -0400

    Add stress test

---
+++ threejs-basic-stress-test/gl-implementation.js
@@ -0,0 +1,194 @@
+/**
+ * Mediator between the worker iframe and the gl implementation
+ */
+class WorkerGLProxy {
+  /**
+   * @param {Element} worker - worker iframe
+   * @param {WebGLContext} gl
+   * @param {number|string} workerId - unique identifier of worker
+   */
+  constructor(worker, gl, workerId) {
+    this.worker = worker;
+    this.gl = gl;
+    this.workerId = workerId;
+
+    this.uncloneables = {};
+
+    this.commandBuffer = [];
+    this.buffering = false;
+
+    this.onMessage = this.onMessage.bind(this);
+    window.addEventListener('message', this.onMessage);
+
+    this.frameEndListener = null;
+  }
+
+  onMessage(e) {
+    const message = e.data;
+    if (message.workerId !== this.workerId) {
+      return;
+    }
+
+    if (this.frameEndListener && message.isFrameEnd) {
+      this.frameEndListener();
+      return;
+    }
+
+    if (this.buffering) {
+      this.commandBuffer.push(message);
+      return;
+    }
+
+    const res = this.executeCommand(message);
+
+    this.worker.postMessage({
+      id: message.id,
+      result: res,
+    }, '*');
+  }
+
+  executeCommand(message) {
+    for (let i = 0; i < message.args.length; i++) {
+      let arg = message.args[i];
+      if (arg.fakeClone) {
+        message.args[i] = this.uncloneables[arg.index];
+      }
+    }
+
+    if (!this.gl[message.name]) {
+      return;
+    }
+
+    const blacklist = {
+      clear: true,
+    };
+
+    if (blacklist[message.name]) {
+      return;
+    }
+
+    if (message.name === 'useProgram') {
+      this.lastUseProgram = message;
+    }
+
+    if (message.name === 'getShaderPrecisionFormat') {
+      console.log(message);
+    }
+
+    let res = this.gl[message.name].apply(this.gl, message.args);
+    if (typeof res === 'object') {
+      this.uncloneables[message.id] = res;
+      res = {fakeClone: true, index: message.id};
+    }
+    return res;
+  }
+
+  executeFrameCommands() {
+    this.buffering = false;
+    for (let message of this.commandBuffer) {
+      this.executeCommand(message);
+    }
+    this.commandBuffer = [];
+  }
+
+  getFrameCommands() {
+    this.buffering = true;
+    if (this.lastUseProgram) {
+      this.commandBuffer.push(this.lastUseProgram);
+    }
+    this.worker.postMessage({name: 'frame', time: Date.now()}, '*');
+    return new Promise((res) => {
+      this.frameEndListener = res;
+    });
+  }
+}
+
+let workerCount = 10;
+let workers = [];
+
+function sleep(ms) {
+  return new Promise((res) => {
+    setTimeout(res, ms);
+  });
+}
+
+async function main() {
+  const canvas = document.querySelector('#glcanvas');
+  const gl = canvas.getContext('webgl');
+  console.log(gl.getParameter(7938));
+
+  for (let i = 1; i <= workerCount; i++) {
+    let frame = document.createElement('iframe');
+    frame.id = `worker${i}`;
+    frame.src = `worker-${i}.html`;
+    document.body.appendChild(frame);
+    workers.push(frame.contentWindow);
+  }
+
+  // If we don't have a GL context, give up now
+
+  if (!gl) {
+    alert('Unable to initialize WebGL. Your browser or machine may not support it.');
+    return;
+  }
+
+  await sleep(1000);
+
+  const functions = [];
+  const constants = {};
+
+  for (let key in gl) {
+    switch (typeof gl[key]) {
+    case 'function':
+      functions.push(key);
+      break;
+    case 'number':
+      constants[key] = gl[key];
+      break;
+    }
+  }
+
+  let proxies = [];
+  for (let i = 0; i < workers.length; i++) {
+    proxies.push(new WorkerGLProxy(workers[i], gl, i + 1));
+  }
+
+  setTimeout(() => {
+    for (let worker of workers) {
+      worker.postMessage({name: 'bootstrap', functions, constants}, '*');
+    }
+    setTimeout(renderFrame, 500);
+  }, 200);
+
+  async function renderFrame() {
+    let start = performance.now();
+
+    // Get all the commands from the worker iframes
+    await Promise.all(proxies.map(proxy => proxy.getFrameCommands()));
+
+    console.log('got commands', performance.now() - start);
+
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);  // Clear to black, fully opaque
+    gl.clearDepth(1.0);                 // Clear everything
+    gl.enable(gl.DEPTH_TEST);           // Enable depth testing
+    gl.depthFunc(gl.LEQUAL);            // Near things obscure far things
+
+    // Clear the canvas before we start drawing on it.
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+    start = performance.now();
+
+    // Execute all pending commands for this frame
+    for (let proxy of proxies) {
+      proxy.executeFrameCommands();
+    }
+
+    let end = performance.now();
+    console.log('executed commands', end - start);
+    requestAnimationFrame(renderFrame);
+  }
+
+}
+
+main();
+
---
+++ threejs-basic-stress-test/worker-28.js
@@ -0,0 +1,59 @@
+const width = 640;
+const height = 480;
+
+workerId = 28;
+
+async function main() {
+  if (!gl) {
+    console.error('Unable to initialize WebGL. Your browser or machine may not support it.');
+    return;
+  }
+
+  const scene = new THREE.Scene();
+  scene.add(new THREE.AmbientLight(0xaaaaaa));
+  var directionalLight = new THREE.DirectionalLight(0xffffff, 1.0);
+  directionalLight.position.x = -20;
+  directionalLight.position.y = -20;
+  directionalLight.position.z = -20;
+  scene.add( directionalLight );
+
+
+  const camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000);
+  camera.position.z = 35;
+
+  // const geometry = new THREE.BoxGeometry();
+  const geometry = new THREE.TorusKnotBufferGeometry( 10, 3, 100, 16 );
+  // const material = new THREE.MeshBasicMaterial({color: 0x00ff00});
+  const material = new THREE.MeshLambertMaterial({color: 0xffa400});
+  let cube = new THREE.Mesh(geometry, material);
+  cube.position.x = -15 + workerId;
+  scene.add(cube);
+
+  let realRenderer = new THREE.WebGLRenderer();
+  realRenderer.setSize(width, height);
+  realGl = realRenderer.getContext();
+
+  const renderer = new THREE.WebGLRenderer({context: gl});
+  renderer.setSize(width, height);
+
+  let then = 0;
+  let done = false;
+  // Draw the scene repeatedly
+  render = function(now) {
+    now *= 0.001;  // convert to seconds
+    const deltaTime = now - then;
+    then = now;
+
+    cube.rotation.x -= (0.2 + workerId / 60) * deltaTime;
+    cube.rotation.y -= 0.2 * deltaTime;
+
+    renderer.render(scene, camera);
+
+    if (done) {
+      realGl = null;
+      realRenderer = null;
+      // delete and clean up realGl
+    }
+    done = true;
+  }
+}
